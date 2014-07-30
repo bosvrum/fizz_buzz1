a491cdfdc9d82d3af2726c0ee04548d4b4cbcfe2
Catalin <catalinboruga@yahoo.com>
2014-07-30 15:02:16 +0200

    Refactoring the solution by extracting the conditional expression to a query method

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,13 @@ BUZZ = 'Buzz'
 BUZZ_NUMBER = 5
 
 def fizzbuzz(the_number)
-	return 'Fizz' if the_number == FIZZ_NUMBER
+	
+	return FIZZ if fizz_number? the_number
 	return 'Buzz' if the_number == 5 || the_number == 10 
 	return "FizzBuzz" if the_number % 3 == 0 && the_number % 5 == 0
     the_number
+end
+
+def fizz_number? the_number
+	the_number == FIZZ_NUMBER
 end
