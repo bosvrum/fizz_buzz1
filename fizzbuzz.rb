eeba4800b1c78912ebc9b8061423c1ad85a5214e
Catalin <catalinboruga@yahoo.com>
2014-07-30 15:08:41 +0200

    Refactoring by exctracting the conditional expresion of Buzz to a query method

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,11 +6,15 @@ BUZZ_NUMBER = 5
 def fizzbuzz(the_number)
 	
 	return FIZZ if fizz_number? the_number
-	return 'Buzz' if the_number == 5 || the_number == 10 
+	return BUZZ if buzz_number? the_number 
 	return "FizzBuzz" if the_number % 3 == 0 && the_number % 5 == 0
     the_number
 end
 
 def fizz_number? the_number
 	the_number == FIZZ_NUMBER
+end
+
+def buzz_number? the_number
+	the_number = BUZZ_NUMBER
 end
