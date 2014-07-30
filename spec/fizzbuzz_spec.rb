4285dabf29ccfc168915732dfbb6c95da339d9e6
Catalin <catalinboruga@yahoo.com>
2014-07-30 12:45:59 +0200

    Specification for the cases that replies Fizz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,5 +4,10 @@ describe "FizzBuzz" do
   it "replies the same number" do
     expect(fizzbuzz(1)).to eq(1)
     expect(fizzbuzz(2)).to eq(2)
+    expect(fizzbuzz(4)).to eq(4)
   end
+  it "replies fizz" do
+  	expect(fizzbuzz(3)).to eq('Fizz')
+  end
+
 end
