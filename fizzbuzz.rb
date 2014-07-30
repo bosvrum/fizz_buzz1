19147bf789ab5e380b9384bb3bddfe53f609cb91
Catalin <catalinboruga@yahoo.com>
2014-07-30 13:15:14 +0200

    More specification for cases that replies Buzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(the_number)
 	return 'Fizz' if the_number == 3 || the_number == 6 || the_number == 9
-	return 'Buzz' if the_number == 5
+	return 'Buzz' if the_number == 5 || the_number == 10 || the_number == 15
     the_number
 	
 end
